c1208b440446d9a8e8e7c12f2df6218cb14fae60
em <moy_garb@bk.ru>
2020-09-21 18:49:12 +0300

    lesson 8

---
+++ Lesson_8/geekshop/adminapp/views.py
@@ -0,0 +1,231 @@
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
+
+from authapp.forms import ShopUserRegisterForm
+from authapp.models import ShopUser
+from mainapp.models import Product, ProductCategory
+from .forms import ProductEditForm, ShopUserAdminEditForm
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from django.db import connection
+
+from django.db.models import F
+from adminapp.forms import ProductCategoryEditForm
+
+
+class UsersListView(ListView):
+    model = ShopUser
+    template_name = 'adminapp/users.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_create(request):
+    title = 'пользователи/создание'
+
+    if request.method == 'POST':
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin:users'))
+    else:
+        user_form = ShopUserRegisterForm()
+
+    content = {'title': title, 'update_form': user_form}
+
+    return render(request, 'adminapp/user_update.html', content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_update(request, pk):
+    title = 'пользователи/редактирование'
+
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:user_update', args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+
+    content = {'title': title, 'update_form': edit_form}
+
+    return render(request, 'adminapp/user_update.html', content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_delete(request, pk):
+    title = 'пользователи/удаление'
+
+    user = get_object_or_404(ShopUser, pk=pk)
+
+    if request.method == 'POST':
+        if user.is_active:
+            user.is_active = False
+        else:
+            user.is_active = True
+        user.save()
+        # user.delete()
+        return HttpResponseRedirect(reverse('admin:users'))
+
+    content = {
+        'title': title,
+        'user_to_delete': user
+    }
+
+    return render(request, 'adminapp/user_delete.html', content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def categories(request):
+    title = 'админка/категории'
+
+    categories_list = ProductCategory.objects.all()
+
+    content = {
+        'title': title,
+        'objects': categories_list
+    }
+
+    return render(request, 'adminapp/categories.html', content)
+
+
+class ProductCategoryCreateView(CreateView):
+    model = ProductCategory
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin:categories')
+    fields = '__all__'
+
+
+class ProductCategoryUpdateView(UpdateView):
+    model = ProductCategory
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin:categories')
+    form_class = ProductCategoryEditForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'категории/редактирование'
+        return context
+
+    def form_valid(self, form):
+        if 'discount' in form.cleaned_data:
+            discount = form.cleaned_data['discount']
+            if discount:
+                self.object.product_set.update(price=F('price') * (1 - discount/100))
+                db_profile_by_type(self.__class__, 'UPDATE', connection.queries)
+        return super().form_valid(form)
+
+
+class ProductCategoryDeleteView(DeleteView):
+    model = ProductCategory
+    template_name = 'adminapp/category_delete.html'
+    success_url = reverse_lazy('admin:categories')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.is_active:
+            self.object.is_active = False
+        else:
+            self.object.is_active = True
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def products(request, pk):
+    title = 'админка/продукт'
+
+    category = get_object_or_404(ProductCategory, pk=pk)
+    products_list = Product.objects.filter(category__pk=pk).order_by('name')
+
+    content = {
+        'title': title,
+        'category': category,
+        'objects': products_list,
+    }
+
+    return render(request, 'adminapp/products.html', content)
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'adminapp/product_read.html'
+
+
+def product_create(request, pk):
+    title = 'продукт/создание'
+    category = get_object_or_404(ProductCategory, pk=pk)
+
+    if request.method == 'POST':
+        product_form = ProductEditForm(request.POST, request.FILES)
+        if product_form.is_valid():
+            product_form.save()
+            return HttpResponseRedirect(reverse('admin:products', args=[pk]))
+    else:
+        # задаем начальное значение категории в форме
+        product_form = ProductEditForm(initial={'category': category})
+
+    content = {'title': title, 'update_form': product_form, 'category': category}
+
+    return render(request, 'adminapp/product_update.html', content)
+
+
+def product_update(request, pk):
+    title = 'продукт/редактирование'
+
+    edit_product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        edit_form = ProductEditForm(request.POST, request.FILES, instance=edit_product)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:product_update', args=[edit_product.pk]))
+    else:
+        edit_form = ProductEditForm(instance=edit_product)
+
+    content = {'title': title, 'update_form': edit_form, 'category': edit_product.category}
+
+    return render(request, 'adminapp/product_update.html', content)
+
+
+def product_delete(request, pk):
+    title = 'продукт/удаление'
+
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        product.is_active = False
+        product.save()
+        return HttpResponseRedirect(reverse('admin:products', args=[product.category.pk]))
+
+    content = {
+        'title': title,
+        'product_to_delete': product
+    }
+
+    return render(request, 'adminapp/product_delete.html', content)
+
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x['sql'], queries))
+    with open('./log.log', 'w') as file:
+        print(f'db_profile {type} for {prefix}: ', file=file)
+        [print(query['sql'], file=file) for query in update_queries]
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_update_productcategory_save(sender, instance, **kwargs):
+    if instance.pk:
+        if instance.is_active:
+            instance.product_set.update(is_active=True)
+        else:
+            instance.product_set.update(is_active=False)
+        db_profile_by_type(sender, 'UPDATE', connection.queries)
---
+++ Lesson_8/geekshop/mainapp/views.py
@@ -0,0 +1,161 @@
+import datetime, random, os, json
+from django.shortcuts import render, get_object_or_404
+from mainapp.models import ProductCategory, Product
+from basketapp.models import Basket
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import JsonResponse
+
+from ordersapp.models import OrderItem, Order
+
+from django.conf import settings
+from django.core.cache import cache
+
+from django.views.decorators.cache import cache_page
+
+
+JSON_PATH = 'mainapp/json'
+
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_active=True)
+        
+def get_category(pk):
+    if settings.LOW_CACHE:
+        key = f'category_{pk}'
+        category = cache.get(key)
+        if category is None:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            cache.set(key, category)
+        return category
+    else:
+        return get_object_or_404(ProductCategory, pk=pk)
+        
+def load_from_json(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r') as infile:
+        return json.load(infile)
+
+# def get_basket(user):
+#     if user.is_authenticated:
+#         return Basket.objects.filter(user=user)
+#     else:
+#         return []
+
+        
+def get_hot_product():
+    products = Product.objects.filter(is_active=True, category__is_active=True)
+    
+    return random.sample(list(products), 1)[0]
+    
+    
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category, is_active=True).exclude(pk=hot_product.pk)[:3]
+    
+    return same_products
+        
+
+        
+def main(request):
+    title = 'главная'  
+    # products = Product.objects.filter(is_active=True, category__is_active=True)[:3]
+    products = Product.objects.filter(is_active=True, category__is_active=True).select_related('category')[:3]
+    content = {
+        'title': title,
+        'products': products,
+        # 'basket': get_basket(request.user),
+    }
+    
+    return render(request, 'mainapp/index.html', content)
+    
+@cache_page(3600)
+def products(request, pk=None, page=1):   
+    title = 'продукты'
+    # links_menu = ProductCategory.objects.filter(is_active=True)
+    # basket = get_basket(request.user)
+           
+    if pk is not None:
+        if pk == 0:
+            category = {
+                'pk': 0,
+                'name': 'все'
+            }
+            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+        else:
+            category = get_category(pk)
+            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
+        
+        paginator = Paginator(products, 2)
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
+        
+        content = {
+            'title': title,
+            'links_menu': get_links_menu(),
+            'category': category,
+            'products': products_paginator,
+            # 'basket': basket,
+        }
+        
+        return render(request, 'mainapp/products_list.html', content)
+    
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+    
+    content = {
+        'title': title,
+        'links_menu': get_links_menu(), 
+        'hot_product': hot_product,
+        'same_products': same_products,
+        # 'basket': basket,
+    }
+    
+    return render(request, 'mainapp/products.html', content)
+    
+    
+def product(request, pk):
+    title = 'продукты'
+    # links_menu = ProductCategory.objects.filter(is_active=True)
+
+    product = get_object_or_404(Product, pk=pk)
+    
+    content = {
+        'title': title, 
+        'links_menu': get_links_menu(), 
+        'product': product, 
+        # 'basket': get_basket(request.user),
+    }
+    return render(request, 'mainapp/product.html', content)
+    
+
+def contact(request):
+    title = 'о нас'
+    
+    locations = load_from_json('contact__locations')
+    
+    content = {
+        'title': title,
+        'locations': locations,
+        # 'basket': get_basket(request.user),
+    }
+    
+    return render(request, 'mainapp/contact.html', content)
+    
+
+def get_product_price(request, name):
+    if request.is_ajax():
+        product = Product.objects.filter(name=name).first()
+        if product:
+            return JsonResponse({'price': product.price})
+        else:
+            return JsonResponse({'price': 0})
